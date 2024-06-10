8a7a6225c650ede2051716eb1b73e786942b04e3
mihstn <clickon.ch@gmail.com>
2024-06-10 21:28:24 +0200

    One Loop to learn

--- main.ts
+++ main.ts
@@ -3,10 +3,17 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.B, function () {
     runOrNot = true
+    finalIndex = names.length
+    for (let index = 0; index <= finalIndex - 1; index++) {
+        basic.showString("" + (names[index]))
+        control.waitMicros(500)
+    }
 })
 let randomName = ""
+let finalIndex = 0
 let runOrNot = false
-let names = ["Michael", "Leo", "Jelena"]
+let names: string[] = []
+names = ["Michael", "Leo", "Jelena"]
 runOrNot = true
 let realFirstChart = ""
 basic.forever(function () {
